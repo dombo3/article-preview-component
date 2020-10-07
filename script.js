e122b53601176cdd3e1e56d123004d600c460b44
Peter Dombovari <dombo.peter@gmail.com>
2020-10-07 16:09:32 +0200

    style 1440 viewport

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let shareContainer = document.getElementById('share-container');
 let contact = document.getElementById('contact');
+let arrow = document.getElementById('arrow');
 
 document.getElementById('unshare-button').addEventListener("click", function() {
   shareContainer.classList.toggle('hidden');
-  contact.classList.toggle('hidden');
+  arrow.classList.toggle('hidden');
 })
 
 document.getElementById('share-button').addEventListener("click", function() {
   shareContainer.classList.toggle('hidden');
-  contact.classList.toggle('hidden');
+  arrow.classList.toggle('hidden');
 });
